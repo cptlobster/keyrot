68719fabb5c79d5171654ba0637f8905fea6753c
Dustin Thomas <stdio@cptlobster.dev>
2024-09-02 14:56:47 -0500

    exec third party programs for images/sounds

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # keyrot
 brainrot "keylogger". checks if input string matches some pre-specified internet meme, displays the associated meme in fullscreen, and plays a sound.
 
+## Dependencies
+
+This package requires `feh` and `sox` to be installed to work properly:
+
+```shell
+# ubuntu/debian
+sudo apt install feh sox libsox-mp3
+
+# arch
+sudo pacman -S feh sox libid3tag libmad twolame
+```
+
+If your audio files are in MP3 format, you will need to add the following dependencies:
+
+```shell
+# ubuntu/debian
+sudo apt install libsox-mp3
+
+# arch
+sudo pacman -S libid3tag libmad twolame
+```
+
 ## Usage
 Compile using make:
 
--- flashbang.c
+++ flashbang.c
@@ -1,22 +1,35 @@
 #include "flashbang.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 // trigger flashbang (display image full screen, play a sound)
 int bang(char *img_name, char *sound_name) {
-    printf("FLASHBANG");
     // TODO: use feh to display an image full screen
-    pid_t p = fork();
-    if (p < 0) {
+    pid_t p_img = fork();
+    if (p_img < 0) {
+        printf("flashbang: fork() returned exit code %d", p_img);
+        exit(1);
+    }
+    if (p_img == 0) {
         printf("Show %s", img_name);
+        char *args[] = {"feh", "--fullscreen", "--auto-zoom", "--image-bg", "white", img_name, '\0'};
+        execvp(args[0], args);
         exit(0);
     }
     // TODO: play a sound
-    pid_t p = fork();
-    if (p < 0) {
+    pid_t p_audio = fork();
+    if (p_audio < 0) {
+        printf("flashbang: fork() returned exit code %d", p_img);
+        exit(1);
+    }
+    if (p_audio == 0) {
         printf("Play %s", sound_name);
+        char *args[] = {"play", "-q", sound_name, '\0'};
+        execvp(args[0], args);
         exit(0);
     }
+    printf("FLASHBANG");
     return 0;
 }
