f549e10f8b0d37ff1908e13f3bd3a0581ef86366
Dustin Thomas <stdio@cptlobster.dev>
2024-09-02 13:59:23 -0500

    fork to execute processes for video/audio

--- flashbang.c
+++ flashbang.c
@@ -1,12 +1,22 @@
 #include "flashbang.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // trigger flashbang (display image full screen, play a sound)
 int bang(char *img_name, char *sound_name) {
     printf("FLASHBANG");
     // TODO: use feh to display an image full screen
-    printf("Show %s", img_name);
+    pid_t p = fork();
+    if (p < 0) {
+        printf("Show %s", img_name);
+        exit(0);
+    }
     // TODO: play a sound
-    printf("Play %s", sound_name);
+    pid_t p = fork();
+    if (p < 0) {
+        printf("Play %s", sound_name);
+        exit(0);
+    }
     return 0;
 }
